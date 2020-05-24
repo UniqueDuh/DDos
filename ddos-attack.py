45c8f8f2272aeb1c937bbeba27da83151ba00c0e
UniqueDuh <64151125+UniqueDuh@users.noreply.github.com>
2020-05-24 21:07:34 +0200

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -20,7 +20,7 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : Akuma"
+print("Author   : Akuma")
 
 print
 ip = raw_input("IP Target : ")
